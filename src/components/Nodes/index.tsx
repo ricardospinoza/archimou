0f96f29af0b3a0bd6139e44bb5317b5357fcf168
Thales Augusto Gelinger <thalesgelinger@Thaless-MacBook-Air.local>
2022-09-16 23:02:16 -0300

    refact: Nodes

--- src/components/Nodes/index.tsx
+++ src/components/Nodes/index.tsx
@@ -1,6 +1,6 @@
-import {View} from 'react-native';
 import {PersonNode} from '../../models/TreeViewModel';
 import {Person} from '../Person';
+import {Container} from './styles';
 
 interface NodesProps {
   nodes: PersonNode[];
@@ -10,22 +10,15 @@ interface NodesProps {
 
 export const Nodes = ({nodes, onLongNodePress, onNodePress}: NodesProps) => {
   return (
-    <View
-      style={{
-        position: 'absolute',
-        height: '100%',
-      }}>
-      {nodes.map(node => {
-        console.log('INSIDE MAP: ', {node});
-        return (
-          <Person
-            key={node.id}
-            value={node}
-            onLongPress={() => onLongNodePress(node)}
-            onPress={() => onNodePress(node)}
-          />
-        );
-      })}
-    </View>
+    <Container>
+      {nodes.map(node => (
+        <Person
+          key={node.id}
+          value={node}
+          onLongPress={() => onLongNodePress(node)}
+          onPress={() => onNodePress(node)}
+        />
+      ))}
+    </Container>
   );
 };
